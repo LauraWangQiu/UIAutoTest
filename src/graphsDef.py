7ee71f2b0e3c7e5eb0cf062cbed34cf48c8f5700
AndresGarcia :) <andrga37@ucm.es>
2025-05-02 16:54:24 +0200

    Class Graph commented

--- src/graphsDef.py
+++ src/graphsDef.py
@@ -18,7 +18,11 @@ class Node:
     def add_transition(self, _transition):
         self.transitions.append(_transition)
 
+    # Im not using this function, but we can keep it just in case
     def remove_transition(self, _node):
+        """
+        Removes the transition to a node
+        """
         for transition in self.transitions:
             if transition.destination == _node:
                 self.transitions.remove(_node)
@@ -39,28 +43,58 @@ class Transition:
         self.condition = _condition
 
     def is_valid(self) -> bool:
+        """
+        Function to check the condition of transition
+        Returns true if condition is valid, false otherwise
+        """
         return self.condition()
     
 
 class Graph:
     """
-    Class Transition contains a condition to go to destination node
+    Class Graph contains the nodes and has functions to go through them
     
     Atributes:
         nodes (list): List of nodes
+        startNode (Node): root node or main node
     """
     def __init__(self):
         self.nodes = []
         self.startNode = None
 
     def add_node(self, _name:str, _image:Path) ->Node:
+        """
+        Adds a node to the graph
+        Returns new node
+        """
         n = Node(_name,_image)
+        # Adds node to the list of nodes
         self.nodes.append(n)
         return n
     
+    def remove_node(self, _node:Node) -> bool:
+        """
+        Removes the node from the graph and removes all transitions pointing to it
+        Returns False if the node was not found or cannot be removed, True otherwise
+        """
+        # If node is not in graph
+        if _node not in self.nodes:
+            print(f"Node {_node.name} is not in graph")
+            return False
+        
+        # Remove all the transitions to _node
+        for node in self.nodes:
+            node.transitions = [t for t in node.transitions if t.destination is not _node]
+        
+        # Remove node from list of nodes
+        self.nodes.remove(_node)
+        print(f"node has been removed")
+        return True
+
     def add_transition(self, _origin, _destination, _condition = None):
         for node in self.nodes:
             if node is _origin:
+                # In case we dont want a condition
                 if _condition is None:
                     node.add_transition(Transition(_destination, lambda:True))
                 else:
@@ -70,7 +104,7 @@ class Graph:
     def set_start_node(self, _node):
         self.startNode = _node
 
-    # Recursive method to visit all the nodes using dfs
+    # Recursive function to visit all the nodes using dfs
     def dfs(self, actual:Node = None, visited:list = None):
         if visited is None:
             visited = set()
@@ -86,7 +120,6 @@ class Graph:
             if transition.is_valid(): # if condition is valid then check it destinations
                 self.dfs(transition.destination, visited)
 
-
 g = Graph()
 n1 = g.add_node("1", Path("imgs/image.png"))
 n2 = g.add_node("2", Path("imgs/image.png"))
@@ -99,5 +132,9 @@ g.add_transition(n1,n2)
 g.add_transition(n1,n3)
 g.add_transition(n3,n4)
 g.add_transition(n3,n5)
+
+g.dfs()
+
+g.remove_node(n3)
 
 g.dfs()
