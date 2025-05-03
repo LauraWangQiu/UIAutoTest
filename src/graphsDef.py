2715a2f51f651b20ccee8723b41f8728b0072b07
CristinaMora <100274708+CristinaMora@users.noreply.github.com>
2025-05-03 15:02:25 +0200

    Graph visualization and some fix

--- src/graphsDef.py
+++ src/graphsDef.py
@@ -4,7 +4,7 @@ class Node:
     """
     Class Node contains information about the image that represents the state, and a list of transitions 
     
-    Atributes:
+    Attributes:
         name (str): State name
         image (Path): Path to image of the state
         transitions (list): List of transitions to change state
@@ -13,29 +13,35 @@ class Node:
     def __init__(self, _name:str, _image:Path):
         self.name = _name
         self.image = _image
-        self.transitions= []
+        self.transitions = []
 
     def add_transition(self, _transition):
+        # Check if the transition already exists
+        for t in self.transitions:
+            if t.destination == _transition.destination and t.condition.__code__.co_code == _transition.condition.__code__.co_code:
+                print(f"Transition to {t.destination.name} already exists.")
+                return
         self.transitions.append(_transition)
 
-    # Im not using this function, but we can keep it just in case
     def remove_transition(self, _node):
         """
         Removes the transition to a node
         """
-        for transition in self.transitions:
-            if transition.destination == _node:
-                self.transitions.remove(_node)
-                break
-
+        self.transitions = [t for t in self.transitions if t.destination != _node]
+    def update_name(self, new_name: str):
+        """
+        Updates the name of the node.
+        """
+        print(f"Changing node name from '{self.name}' to '{new_name}'")
+        self.name = new_name
 
 class Transition:
     """
     Class Transition contains a condition to go to destination node
     
-    Atributes:
+    Attributes:
         destination (Node): Destination node
-        condition (lambda): Condition to fo to destination
+        condition (lambda): Condition to go to destination
     """
 
     def __init__(self, _destination, _condition):
@@ -44,7 +50,7 @@ class Transition:
 
     def is_valid(self) -> bool:
         """
-        Function to check the condition of transition
+        Check the condition of transition
         Returns true if condition is valid, false otherwise
         """
         return self.condition()
@@ -54,10 +60,11 @@ class Graph:
     """
     Class Graph contains the nodes and has functions to go through them
     
-    Atributes:
+    Attributes:
         nodes (list): List of nodes
         startNode (Node): root node or main node
     """
+
     def __init__(self):
         self.nodes = []
         self.startNode = None
@@ -67,6 +74,11 @@ class Graph:
         Adds a node to the graph
         Returns new node
         """
+        for node in self.nodes:
+            if node.name == _name:
+                print(f"Node with name '{_name}' already exists.")
+                return None  
+
         n = Node(_name,_image)
         # Adds node to the list of nodes
         self.nodes.append(n)
@@ -88,21 +100,32 @@ class Graph:
         
         # Remove node from list of nodes
         self.nodes.remove(_node)
-        print(f"node has been removed")
+        print(f"Node '{_node.name}' has been removed")
         return True
 
-    def add_transition(self, _origin, _destination, _condition = None):
-        for node in self.nodes:
-            if node is _origin:
-                # In case we dont want a condition
-                if _condition is None:
-                    node.add_transition(Transition(_destination, lambda:True))
-                else:
-                    node.add_transition(Transition(_destination, _condition))
-                break
-    
+    def add_transition(self, _origin, _destination, _condition=None):
+        if _origin not in self.nodes or _destination not in self.nodes:
+            print("Origin or destination node is not in the graph.")
+            return
+
+        condition = _condition if _condition is not None else lambda: True
+        _origin.add_transition(Transition(_destination, condition))
+
     def set_start_node(self, _node):
+        """
+        Sets the start node of the graph.
+        """
+        if _node is None:
+            print("Error: The start node cannot be None.")
+            return False
+
+        if _node not in self.nodes:
+            print(f"Error: The node '{_node.name}' is not part of the graph.")
+            return False
+
         self.startNode = _node
+        print(f"Start node set to '{_node.name}'.")
+        return True
 
     # Recursive function to visit all the nodes using dfs
     def dfs(self, actual:Node = None, visited:set = None) -> set:
@@ -124,6 +147,17 @@ class Graph:
         
         # Returns the visited nodes
         return visited
+    def update_node_name(self, node: Node, new_name: str):
+            """
+            Updates the name of a node and ensures all transitions pointing to it remain valid.
+            """
+            if node not in self.nodes:
+                print(f"Node '{node.name}' is not in the graph.")
+                return False
+             # Update the node's name
+            node.update_name(new_name)
+            print(f"Node name updated to '{new_name}'")
+            return True
 
 # --------------------
 # Example of use
