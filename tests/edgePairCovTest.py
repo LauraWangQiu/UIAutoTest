fd2ad46d4760e24dde8efe99edb26df37944a9f8
Paulalemany <palemany@ucm.es>
2025-05-08 16:47:14 +0200

    Time to test the Tests

--- tests/edgePairCovTest.py
+++ tests/edgePairCovTest.py
@@ -3,7 +3,7 @@ from src.test import Test
 class EdgePairCovTest(Test):
     def __init__(self, selected_executable=None):
         super().__init__("EPC Test", selected_executable)
-        self.PairCovList = set()
+        self.EdgePairCovList = {}   #Dictionary of duplicated edges
         # Variables and modifiable parameters
 
     def run(self):
@@ -12,18 +12,17 @@ class EdgePairCovTest(Test):
 
     def on_graph_step(self, element):
         print(f"Element: {element}")
-        # ...
-        #for transitions in element:
-
-        edge_count = {}  
-        for transition in element.transitions:
-            dest_name = transition.destination.name #Destination of the node
-            edge_count[dest_name] = edge_count.get(dest_name, 0) + 1
-
-        for dest_name, count in edge_count.items():
-            if count > 1:
-                if (element.name, dest_name) not in self.duplicate_edges:
-                    self.duplicate_edges[(element.name, dest_name)] = count
-                    print(f"Multiple transitions ({count}) from '{element.name}' to '{dest_name}'")
-
         
+         # It is a node
+        if hasattr(element, "name"):
+            edge_count = {}
+            for transition in element.transitions:
+                dest_name = transition.destination.name #Destination of the node
+                edge_count[dest_name] = edge_count.get(dest_name, 0) + 1
+
+            for dest_name, count in edge_count.items():
+                if count > 1:
+                    if (element.name, dest_name) not in self.EdgePairCovList:
+                        self.EdgePairCovList[(element.name, dest_name)] = count
+                        print(f"Multiple transitions ({count}) from '{element.name}' to '{dest_name}'")
+       
--- tests/selfLoopTest.py
+++ tests/selfLoopTest.py
@@ -4,9 +4,6 @@ class SelfLoopTest(Test):
     def __init__(self, selected_executable=None):
         super().__init__("SP Test", selected_executable)
         self.selfLoopList = set()
-        # TO REMOVE
-        self.selfLoopList.add("selfLoop")
-        self.selfLoopList.add("selfLoop2")
 
     def run(self):
         super().run()
@@ -15,8 +12,16 @@ class SelfLoopTest(Test):
     def on_graph_step(self, element):
         print(f"Element: {element}")
 
-        for transition in element.transitions:
-            if transition.destination is element:      #If the destination is the same node
-                print("SelfLoop in node: {element.name}")   
-                self.selfLoopList(element)
-                break
+        # It is a node
+        if hasattr(element, "name"):
+            #See all the transitions of the node
+            for transition in element.transitions:
+                if transition.destination is element:      #If the destination is the same node
+                    print("SelfLoop in node: {element.name}")   
+                    self.selfLoopList(element)
+                    break
+
+        # It is a transition
+        #elif hasattr(element, "destination"):
+
+        
