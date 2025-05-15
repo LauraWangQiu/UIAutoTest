47e29c6ddca939b076bd4e64ff5305d858184a16
Paigro <paigro9@gmail.com>
2025-05-15 02:01:33 +0200

    Prime Path Coverage Test.
    The test is done but not the writing.

--- tests/primePathCovTest.py
+++ tests/primePathCovTest.py
@@ -1,14 +1,72 @@
 from src.test import Test
 
 class PrimePathCovTest(Test):
-    def __init__(self, graph=None):
+    def __init__(self, graph = None):
         super().__init__("PPC Test")
         self.graph = graph
-        # Variables and modifiable parameters
+        # Variables and modifiable parameters:
 
     """
         Overrides the parent run method
     """
     def run(self):
         print("Running " + self.name + ".")
-        # Do something with the generated graph
+        self.execute_test()
+        # Do something with the generated graph:
+        
+    # Prime Path Coverage Test:
+    def execute_test(self) -> set:
+        all_paths = []
+        prime_paths = []
+
+        # Get all the paths
+        for node in self.graph.nodes:
+            self.dfs(node, set(), [], all_paths)
+        
+        # Filter and keep the prime paths only.
+        for referencePath in all_paths:
+            prime = True
+
+            for otherPath in all_paths:
+                if referencePath != otherPath:
+                    if self.is_subpath(referencePath, otherPath):
+                        prime = False
+                        break
+            if prime:
+                prime_paths.append(referencePath)
+
+        return prime_paths
+
+    # DFS method to make all the paths.
+    def dfs(self, current, visited, path, all_paths):
+        visited.add(current)
+        path.append(current)
+
+        for transition in current.transitions:
+            next_node = transition.destination
+            if next_node not in visited:
+                self.dfs(next_node, visited.copy(), path.copy(), all_paths)
+            elif next_node == path[0]:
+                all_paths.append(path + [next_node])
+
+        all_paths.append(path)
+
+    # Checks if the possibleSubPath is a sub-path of the referencePath.
+    def is_subpath(self, posibleSubPath, referencePath):
+        # If the possible is bigger than the reference then it is not a sub-path.
+        if len(posibleSubPath) > len(referencePath):
+            return False
+        # Checks if the nodes in reference are in the posible.
+        for i in range(len(referencePath) - len(posibleSubPath) + 1):
+            match = True
+            for j in range(len(posibleSubPath)):
+                if referencePath[i + j] != posibleSubPath[j]:
+                    match = False
+                    break
+            if match:
+                return True
+        return False
+
+    # TODO: PAIGRO HERE.
+    def write_solution(self):
+        pass
